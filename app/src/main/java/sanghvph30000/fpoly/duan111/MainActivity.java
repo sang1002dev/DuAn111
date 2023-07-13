ee941777b73f6cfbe567c7d208be57becc468119
sang1002dev <hoangsang10022000@gmail.com>
2023-07-13 15:12:53 +0700

    Initial commit

---
+++ app/src/main/java/sanghvph30000/fpoly/duan111/MainActivity.java
@@ -0,0 +1,14 @@
+package sanghvph30000.fpoly.duan111;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
