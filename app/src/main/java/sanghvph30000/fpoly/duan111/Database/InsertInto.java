9fb02ad3cfcfbe157cc19e3cf29165f9429f105c
maithanh1886 <maithanh1886@gmail.com>
2023-07-14 10:01:51 +0700

    login thanh

---
+++ app/src/main/java/sanghvph30000/fpoly/duan111/Database/InsertInto.java
@@ -0,0 +1,8 @@
+package sanghvph30000.fpoly.duan111.Database;
+
+public class InsertInto {
+    public static final String insert_chucvu = "INSERT INTO ChucVu VALUES (1,'Quản Lý'),(2,'Khách Hàng')";
+    public static final String insert_user = "INSERT INTO User VALUES (1,'Quản lý','admin',1,'admin', 098386828, 2003)," +
+            "(2,'Văn Sang','cus',2,'cus', '0968786843', 2000)";
+}
+
